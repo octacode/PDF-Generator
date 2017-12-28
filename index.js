3ac21b979b060585a75b5edb6452cadb1b3084ef
octacode <allblue.shasha@gmail.com>
2017-12-28 12:58:22 +0530

    Added aws-sdk

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     expressSanitizer = require('express-sanitizer'),
     PDFDocument = require ('pdfkit'),
+    AWS = require('aws-sdk'),
     doc = new PDFDocument,
+    s3 = new AWS.S3(),
+    nodemailer = require('nodemailer'),
     port = process.env.PORT || 1313;
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,6 +16,29 @@ const fs = require('fs');
 
 app.get('/', (req, res)=>{
     res.render('new.ejs');
+    /*
+    var transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+          user: 'internkart123@gmail.com',
+          pass: 'W!qXV?*#Av7Gd&Mu'
+        }
+      });
+
+      const mailOptions = {
+        from: 'internkart123@gmail.com',
+        to: 'allblue.shasha@gmail.com',
+        subject: 'Testing it out',
+        html: '<h1>Kaizoku Ou Ni Ore Wa Naru</h1>'
+      };
+
+      transporter.sendMail(mailOptions, function (err, info) {
+        if(err)
+          console.log(err)
+        else
+          console.log(info);
+      });
+      */
 });
 
 app.post('/', (req, res)=>{
@@ -115,11 +141,27 @@ app.post('/', (req, res)=>{
     doc.moveDown(0.5);
     doc.text('Facebook: ' + facebook, {align:'center', underline: true, link: facebook});
 
-    doc.pipe(fs.createWriteStream('testing.pdf'));
-
     doc.end();
 
-    res.render('complete.ejs');
+    var params = {
+      Key : email_id+'.pdf',
+      Body : doc,
+      Bucket: 'resume-testf6342644-c9a5-40aa-821d-4ce624747f38',
+      ContentType : 'application/pdf'
+    }
+    s3.upload(params, function(err, data) {
+      if(err)
+        console.log(err);
+      else {
+        console.log("Successfully uploaded data");
+//Do the insertion into db here and conflict strategy as REPLACE.
+        console.log(data.location);
+        res.render('complete.ejs');
+      }
+    });
+    // s3.upload(params, (err, res)=>{
+    //     res.render('complete.ejs');
+    // });
 });
 
 app.listen(port, function(){
